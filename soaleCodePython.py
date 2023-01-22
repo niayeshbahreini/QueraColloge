09bdd77dacf44ea67d9d58bedac63e6eefe39715
niayeshbhrn <niayeshbhrn@yahoo.com>
2023-01-22 21:57:31 +0330

    Tamrindorei

---
+++ soaleCodePython.py
@@ -0,0 +1,21 @@
+def solve(path):
+    count = 0
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            if not len(line) == 0 and not line.startswith('#'):
+                count += 1
+    return count
+    
+
+
+def solve(path):
+    i=0
+    with open(path) as file:
+        for row in file:
+            line=row.strip()
+            if line !='' and line.startswith('#')== False:
+                i+=1
+    return i
+
